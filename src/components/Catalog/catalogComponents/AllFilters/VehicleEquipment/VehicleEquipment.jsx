3e082ff462a52a6fe9d77dc2d7bdbf19a0c1d96e
YakymenkoOleksandr <93155369+YakymenkoOleksandr@users.noreply.github.com>
2024-09-27 04:38:13 +0300

    addcomit7

--- src/components/Catalog/catalogComponents/AllFilters/VehicleEquipment/VehicleEquipment.jsx
+++ src/components/Catalog/catalogComponents/AllFilters/VehicleEquipment/VehicleEquipment.jsx
@@ -9,31 +9,31 @@ function VehicleEquipment() {
       <div className={css.wrapperForFilterIcons}>
         <div className={css.buttonOfFilter}>
           <svg className={css.iconForVehicleEquipment}>
-            <use href="../../../../../../public/icons/symbol-defs.svg#icon-wind"></use>
+            <use href="../../../../../public/icons/symbol-defs.svg#icon-wind"></use>
           </svg>
           <p className={css.textInFilterButton}>AC</p>
         </div>
         <div className={css.buttonOfFilter}>
           <svg className={css.iconForVehicleEquipment}>
-            <use href="../../../../../../public/icons/symbol-defs.svg#icon-diagram"></use>
+            <use href="../../../../../public/icons/symbol-defs.svg#icon-diagram"></use>
           </svg>
           <p className={css.textInFilterButton}>Automatic</p>
         </div>
         <div className={css.buttonOfFilter}>
           <svg className={css.iconForVehicleEquipment}>
-            <use href="../../../../../../public/icons/symbol-defs.svg#icon-Group"></use>
+            <use href="../../../../../public/icons/symbol-defs.svg#icon-Group"></use>
           </svg>
           <p className={css.textInFilterButton}>Kitchen</p>
         </div>
         <div className={css.buttonOfFilter}>
           <svg className={css.iconForVehicleEquipment}>
-            <use href="../../../../../../public/icons/symbol-defs.svg#icon-tv"></use>
+            <use href="../../../../../public/icons/symbol-defs.svg#icon-tv"></use>
           </svg>
           <p className={css.textInFilterButton}>TV</p>
         </div>
         <div className={css.buttonOfFilter}>
           <svg className={css.iconForVehicleEquipment}>
-            <use href="../../../../../../public/icons/symbol-defs.svg#icon-bi_droplet"></use>
+            <use href="../../../../../public/icons/symbol-defs.svg#icon-bi_droplet"></use>
           </svg>
           <p className={css.textInFilterButton}>Bathroom</p>
         </div>
